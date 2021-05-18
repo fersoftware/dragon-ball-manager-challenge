d380c1ba2db671d3efed31ee8110bb5e2caee7e0
Fersoftware <fersoftware@gmail.com>
2021-05-18 15:43:04 -0300

    Adding test Action.spec.js

--- cypress/integration/Action.spec.js
+++ cypress/integration/Action.spec.js
@@ -1,40 +1,26 @@
-import React from 'react'
-import { render, fireEvent } from '@testing-library/react'
-import '@testing-library/jest-dom/extend-expect'
-import Action from './Action'
+// <reference types="cypress" />
 
-//mocks
-import { profile } from '../../mocks/profile.json';
-import profileSuccess from '../../mocks/profileSuccess.json';
+const config = {
+    'project' : '/dragon-ball-manager'
+}
 
-it('Should render Action', () => {
-    const { container, debug, getByText } = render(<Action balls={profile.balls} />)
-    const button = getByText('Invocar').closest('button')
+context('Testing shenlong invocation', () => {
+    before(()=>{
+        cy.visit(config.project)
+    })
 
-    expect(getByText('Invocar shenlong')).toBeInTheDocument()
-    expect(button).toBeInTheDocument()
-    expect(container).toMatchSnapshot()
-})
+    //Execute Terminal: REACT_APP_PROFILE=SUCCESS npm start
+    //Removal 'skip'
+    it.skip('Should invocate if user have 7 dragon balls', () => {
+        cy.get('[data-testid="card-shenlong"]').should('exist')
+        cy.get('[data-testid="invoke-button"]').click()
+        cy.get('[data-testid="shenlong"]').should('exist')
+    })
 
-it('Should open modal if user dont have all dragon balls', () => {
-    const { debug, getByText } = render(<Action balls={profile.balls} />)
-    const button = getByText('Invocar').closest('button')
-
-    fireEvent.click(button)
-    expect(getByText('Você não tem todas as esferas para invocar o shenlong')).toBeInTheDocument()
-
-    const backButton = getByText('Voltar').closest('button')
-    fireEvent.click(backButton)
-
-    expect(getByText('Invocar shenlong')).toBeInTheDocument()
-})
-
-it('Should show shenlong', () => {
-    const { container, debug, getByText, getByTestId } = render(<Action balls={profileSuccess.profile.balls} />)
-    const button = getByText('Invocar').closest('button')
-
-    fireEvent.click(button)
-    expect(getByTestId('shenlong')).toBeInTheDocument()
-
-    expect(container).toMatchSnapshot()
+    it('Should not invocate if user doesnt have 7 dragon balls', () => {
+        cy.get('[data-testid="card-shenlong"]').should('exist')
+        cy.get('[data-testid="invoke-button"]').click()
+        cy.get('[data-testid="modaltext"]').should('contain.text', 'Você não tem todas as esferas para invocar o shenlong')
+        cy.get('[data-testid="back"]').click()
+    })
 })
